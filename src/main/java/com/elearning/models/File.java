44061c19fb16dd154e0305abe1fb0bf915135684
maaloum <maaloumali1@gmail.com>
2024-09-11 17:26:54 -0400

    File Creation

---
+++ src/main/java/com/elearning/models/File.java
@@ -0,0 +1,9 @@
+package com.elearning.models;
+
+public class File {
+    private String name;
+    private String path;
+    private String type;
+    private String size;
+    private String url;
+}
